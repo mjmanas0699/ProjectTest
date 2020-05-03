1a125f59406a35ab8de6c13971100ed7728431ef
manas jain <mjamnas0699@gmail.com>
2020-05-04 00:58:30 +0530

    i do some miskate please retrive prevois one

--- a.py
+++ a.py
@@ -1,2 +1,1 @@
-print()[D"")[D[DHello) [D[D")[D [D)[D)
-input()[Dg)[D  0000 0000[2~)[D)[D cf[B[D s [B[D[D sd[D [D)[D"")[D[Dhello"[A)[B) [D[)[2~[2~[2~[2~[D)
+manas jain 2nd time
